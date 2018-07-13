b169a448b4de71d5cc4e69d3958ad9c6785c426d
kurian <kurianck.mail@gmail.com>
2018-07-13 23:18:38 +0530

    get and write
        if the given string is a url,then it will get the package and
        put it in a directory
    
        next: directory has to be in the project folder like node.

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
@@ -10,6 +12,16 @@ import (
 func main() {
 	if len(os.Args) > 1 {
 		url := os.Args[1]
+		client := &http.Client{}
+		resp, err := client.Get(url)
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		f, err := os.Create("/Users/kurian/Downloads/hello.zip")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		f.Write(body)
 		fmt.Printf("%s", url)
 	}
 	return
